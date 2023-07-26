bc9ae341cdfba3eefda0cf34c8091f9f9d587b88
DeiviRep <patzivargasdavid@gmail.com>
2023-07-26 16:05:43 -0400

    Configuracion de VITE para despliegue en Github

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // https://github.com/DeiviRep/frontend-chord-app
+  base: "https://DeiviRep.github.io/frontend-chord-app/",
   plugins: [react()],
-})
+});
